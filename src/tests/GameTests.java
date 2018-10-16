7018c7fb8e70d23e3606946c4a8f1130ec38d7e5
maxburton <maxkburton@gmail.com>
2018-10-16 17:16:24 +0100

    Added Win Loss Tie tests

--- src/tests/GameTests.java
+++ src/tests/GameTests.java
@@ -14,4 +14,18 @@ public final class GameTests {
 		final Result actual = main.GameFunctions.getResult(Choice.ROCK,Choice.SCISSORS);
 		Assert.assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void doesUserLose() {
+		final Result expected = Result.LOSS;
+		final Result actual = main.GameFunctions.getResult(Choice.PAPER,Choice.SCISSORS);
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void doesUserTie() {
+		final Result expected = Result.TIE;
+		final Result actual = main.GameFunctions.getResult(Choice.SCISSORS,Choice.SCISSORS);
+		Assert.assertEquals(expected, actual);
+	}
 }
