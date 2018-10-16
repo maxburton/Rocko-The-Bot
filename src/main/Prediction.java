fd8b80e5d2f09a181c8d901dfe6953ba9556fe1f
maxburton <maxkburton@gmail.com>
2018-10-16 17:34:10 +0100

    main updated

--- src/main/Prediction.java
+++ src/main/Prediction.java
@@ -1,5 +1,11 @@
 package main;
 
+import main.GameFunctions.Choice;
+
 public class Prediction {
 
+	public static Choice choosePlay() {
+		Choice choice = null;
+		return choice;
+	}
 }
--- src/main/Run.java
+++ src/main/Run.java
@@ -1,9 +1,46 @@
 package main;
 
+import java.util.Scanner;
+
+import main.GameFunctions.Choice;
+
 public class Run {
 
 	static void main(String[] args){
-		
-		
+		Scanner s = new Scanner(System.in);
+		boolean playing = true;
+		while(playing) {
+			System.out.println("Choose a play: 1. Rock, 2. Paper, 3. Scissors, 0. Quit\n");
+			String input = s.nextLine();
+			int play = -1;
+			Choice choice = Choice.ROCK;;
+			if(input != "1" || input != "2" || input != "3" || input != "0") {
+				play = Integer.parseInt(input);
+			}else {
+				System.out.println("Invalid Input\n");
+				continue;
+			}
+			switch(play) {
+			case 1:
+				choice = Choice.ROCK;
+				break;
+			case 2:
+				choice = Choice.PAPER;
+				break;
+			case 3:
+				choice = Choice.SCISSORS;
+				break;
+			case 0:
+				playing = false;
+				System.out.println("Cya\n");
+				continue;
+			default:
+				choice = Choice.ROCK;
+				break;
+			}
+			Choice botChoice = Prediction.choosePlay();
+			System.out.println(GameFunctions.getResult(choice, botChoice).toString());
+		}
+		s.close();
 	}
 }
